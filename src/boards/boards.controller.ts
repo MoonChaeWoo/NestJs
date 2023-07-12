261954a39180619bba4d05ef546fcf17706fd711
tyche0529 <tyche0529@gmail.com>
2023-07-12 14:30:24 +0900

    나머지 기능 Read, Update, Delete 기능 구성

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, ValidationPipe, Post, Body } from '@nestjs/common';
+import { Controller, Get, Query, ValidationPipe, Post, Body, Param } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './boards.model';
 import { CreatBoardDto } from './dto/createBoardDto';
@@ -7,16 +7,10 @@ import { CreatBoardDto } from './dto/createBoardDto';
 export class BoardsController {
     constructor(private readonly boardsService:BoardsService){}
 
-    @Get('/test')
-    GetAllTask() : string{
-        return this.boardsService.testMethod();
-    }
-
-    @Get('/getAllBoard')
-    getAllBoard() : Board[]{
-        return this.boardsService.getAllBoard();
-    }
-
+    // @Body(), @Query(), @Param() = 파라미터를 비워두면 모두 가져온다는 의미.
+    // @Query('id') id : string = key가 id에 대한 것만 가져온다는 의미. 나머지 데코레이터도 동일.
+    // -------------------------- CRUD ---------------------------
+    // --------------------- Create GET POST ---------------------
     @Get('/insertGetBoard')
     createGetBoard(@Query(new ValidationPipe()) board : CreatBoardDto) : Board{
         return this.boardsService.createBoard(board);
@@ -27,4 +21,65 @@ export class BoardsController {
         return this.boardsService.createBoard(board);
     }
 
+    // ---------------------- Read GET POST -----------------------
+    @Get('/getAllBoard')
+    getAllReadBoard() : Board[]{
+        return this.boardsService.getAllReadBoard();
+    }
+
+    // key=value 형식으로 여러 개의 매개변수를 전달
+    // 쿼리 매개변수는 주로 필터링, 페이징, 정렬 등의 검색 조건을 전달할 때 사용
+    @Get('/readGetBoard')
+    readGetQueryBoard(@Query('id') id : string) : Board{
+        return this.boardsService.getOneReadBoard(id);
+    }
+
+    // 주로 리소스의 식별자나 고유한 값을 전달할 때 사용
+    @Get('/readGetBoard/:id')
+    readGetParamBoard(@Param('id') id : string) : Board{
+        return this.boardsService.getOneReadBoard(id);
+    }
+
+    @Post('/readPostBoard')
+    readPostBoard(@Body('id') id : string) : Board{
+        return this.boardsService.getOneReadBoard(id);
+    }
+
+    // ---------------------- Update GET POST -----------------------
+    // key=value 형식으로 여러 개의 매개변수를 전달
+    // 쿼리 매개변수는 주로 필터링, 페이징, 정렬 등의 검색 조건을 전달할 때 사용
+    @Get('/updateGetBoard')
+    updateGetQueryBoard(@Query() board : Board) : Board[]{
+        return this.boardsService.getOneUpdateBoard(board);
+    }
+
+    // 주로 리소스의 식별자나 고유한 값을 전달할 때 사용
+    @Get('/updateGetBoard/:id')
+    updateGetParamBoard(@Param() board : Board) : Board[]{
+        return this.boardsService.getOneUpdateBoard(board);
+    }
+
+    @Post('/updatePostBoard')
+    updatePostBoard(@Body() board : Board) : Board[]{
+        return this.boardsService.getOneUpdateBoard(board);
+    }
+
+    // ---------------------- Delete GET POST -----------------------
+    // key=value 형식으로 여러 개의 매개변수를 전달
+    // 쿼리 매개변수는 주로 필터링, 페이징, 정렬 등의 검색 조건을 전달할 때 사용
+    @Get('/deleteGetBoard')
+    deleteGetQueryBoard(@Query('id') id : string) : Board[]{
+        return this.boardsService.getOneDeleteBoard(id);
+    }
+
+    // 주로 리소스의 식별자나 고유한 값을 전달할 때 사용
+    @Get('/deleteGetBoard/:id')
+    deleteGetParamBoard(@Param('id') id : string) : Board[]{
+        return this.boardsService.getOneDeleteBoard(id);
+    }
+
+    @Post('/deletePostBoard')
+    deletePostBoard(@Body('id') id : string) : Board[]{
+        return this.boardsService.getOneDeleteBoard(id);
+    }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,23 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { Board } from './boards.model';
-import {v1 as uuid} from 'uuid';
+import { v1 as uuid } from 'uuid';
 import { CreatBoardDto } from './dto/createBoardDto';
 
 @Injectable()
 export class BoardsService {
     private boards : Board[] = [];
 
-    getAllBoard() : Board[]{
-        return this.boards;
-    }
-
+    // ------------------- Create ---------------------
     createBoard(creatBoardDto : CreatBoardDto) : Board{
         const board : Board = { id : uuid(), ...creatBoardDto};
         this.boards.push(board);
         return board;
     }
 
-    testMethod() : string{
-        return 'boardtest';
+    // -------------------- Read ----------------------
+    getAllReadBoard() : Board[]{
+        return this.boards;
+    }
+
+    getOneReadBoard(id : string) : Board{
+        const findItem : Board = this.boards.find(v => v.id === id);
+        return findItem;
+    }
+
+    // -------------------- Update ---------------------
+    getOneUpdateBoard(board : Board) : Board[]{
+        const mapBoard : Board[] = this.boards.map(v => v.id === board.id ? ({v, ...board}) : v);
+        this.boards = mapBoard;
+        return this.boards;
+    }
+
+    // -------------------- Delete ---------------------
+    getOneDeleteBoard(id : string) : Board[]{
+        const filterBoard : Board[] = this.boards.filter(v => v.id !== id);
+        this.boards = filterBoard;
+        return this.boards;
     }
 }
