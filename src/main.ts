518daca12705b158451ca22498afc55428b2ade3
tyche0529 <tyche0529@gmail.com>
2023-07-23 02:32:20 +0900

    프록시 특정 포트 허용

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
+  app.enableCors({
+    origin: 'http://127.0.0.1:3000',
+    credentials: true,
+  });
+  const port = 3001;
   await app.listen(port);
   Logger.log(`Nest Js Start Port =====> ${port}`);
 }
